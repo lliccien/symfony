7139055ee821f0ec169d7bce93ffc4904e6150e4
Ludwring Liccien <ludwring.liccien@gmail.com>
2016-08-11 23:52:29 -0400

    Instalacion y configuracion de DataFixturesBundle y AliceBundle para trabajar fixtures y faker

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,6 @@ class AppKernel extends Kernel
                 new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
                 new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
                 new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
-                new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle(),
                 new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         );
 
@@ -36,6 +35,11 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
+            $bundles[] = new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle();
+
         }
 
         return $bundles;
